61e800ca37ac301746d3db3db4ad88f3af9481c0
me <my@email.com>
2025-02-10 16:21:45 +0100

    morho and stormy

---
+++ index.js
@@ -0,0 +1,40 @@
+var playing = false;
+var playlist = [];
+
+
+$(".music-container").on("click", function(){
+
+    if (playlist.length !== 0){
+        playlist[0].pause();
+        playing = false;
+        playlist = [];
+    }
+
+    var music = new Audio("music/"+ $(this).attr("id") +".mp3");
+    playlist.push(music);
+    console.log("music selected");
+    //play_music(playlist[0]);
+});
+
+
+$(".play-button").on("click", function(){
+    play_music(playlist[0]);
+});
+
+
+$(".back-button").on("click", function(){
+    playlist[0].fastSeek(0);
+});
+
+
+function play_music(music){
+    if (playing === false){
+        music.play();
+        playing = true;
+    }
+    else{
+        music.pause();
+        playing = false;
+    }     
+}
+
